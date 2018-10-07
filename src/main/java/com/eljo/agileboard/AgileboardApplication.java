accbf03d8642f43d5b5f3f51783b237581654eb5
eljog <eljo.p.george@gmail.com>
2018-10-06 20:47:42 -0500

    Springboot initial

---
+++ src/main/java/com/eljo/agileboard/AgileboardApplication.java
@@ -0,0 +1,12 @@
+package com.eljo.agileboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgileboardApplication.class, args);
+	}
+}
