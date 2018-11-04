60bf08c96b6821205e2538778f98ce9d51430309
Eljo George <eljo.p.george@gmail.com>
2018-11-04 13:11:46 -0600

    Update configure_app_service.sh
    * Make the service script executable

--- scripts/configure_app_service.sh
+++ scripts/configure_app_service.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# Make the service script executable
+chmod 500 /apps/agileboard/agileboard.sh
+
 # Register the the app as a Service
 ln -s /apps/agileboard/agileboard.sh /etc/init.d/agileboard
