6e2cab908ca6530b7122e74e2753d2aea24aad72
Eljo George <eljo.p.george@gmail.com>
2018-11-04 13:35:26 -0600

    Update stop_app.sh
    * Attempt to stop App Service only if running

--- scripts/stop_app.sh
+++ scripts/stop_app.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-# Stop App Service
+# Stop App Service if running
+if (( $(ps -ef | grep -v grep | grep "java -Dspring.profiles.active=aws -jar /apps/agileboard/agileboard-0.1.jar" | wc -l) > 0 ))
+then
 service agileboard stop
+else
+echo agileboard is not running
+fi
